d41440b7476022d2bd4b9c5aad04bce49056a88c
Rafael Toresani <rtoresani@gmail.com>
2024-04-22 20:03:00 -0300

    initial commit

---
+++ src/main/java/com/rtoresani/GymManagerbackend/GymManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rtoresani.GymManagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtoresani/GymManagerbackend/GymManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtoresani.GymManagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
